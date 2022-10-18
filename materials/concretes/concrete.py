388d61e2dfba059ffa2af131a13c91e17e0fc116
DanielGMorenaFhecor <dgm@fhecor.es>
2022-10-18 10:18:50 +0200

    Base concrete

--- materials/concretes/concrete.py
+++ materials/concretes/concrete.py
@@ -1,18 +1,35 @@
-# La clase CONCRETE es hija de la clase MATERIAL
-from materials.material import Material
 from abc import abstractmethod
+from materials.material import Material
 
 class Concrete(Material):
+    """Base class for all concrete materials"""
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, density=2400.0):
+        """
+        Initializes a new instance of a concrete material
 
-        # Invoco al constructor padre y le paso el nombre
-        super().__init__(name,density=2400) # Density=2400kg/m3
+        Parameters
+        ----------
+        name : str
+            name of the concrete material
+        density : float
+            concrete density in kg/m3 (default is 2400.0kg/m3)
 
-    @abstractmethod
-    def Ecm(self) -> float:
-        pass
+        Raises
+        ------
+        ValueError
+            if densify is less than zero
+        """
 
+        super().__init__(name,density)
+
+    @property
     @abstractmethod
     def fck(self) -> float:
-        pass
+        """Returns the concrete compressive strength in MPa"""
+
+
+    @property
+    @abstractmethod
+    def Ecm(self) -> float:
+        """Returns the concrete modulus of elasticity in MPa"""
--- materials/material.py
+++ materials/material.py
@@ -1,9 +1,37 @@
 from abc import ABC
 
-# La clase PADRE de todos los Materiales
 class Material(ABC):
+    """Base class for all materials"""
 
-    # Únicamente (de momento) va a tener una propiedad de nombre
     def __init__(self, name: str, density: float):
-        self.name = name
-        self.density=density
+        """
+        Initializes an instance of a new material
+        
+        Parameters
+        ----------
+        name : str
+            name of the material
+        density : float
+            density of the material in kg/m3
+
+        Raises
+        ------
+        ValueError
+            if density is less than z
+        """
+
+        if density < 0:
+            raise ValueError(f'Density cannot be less than zero. Current {density}')
+            
+        self._name = name
+        self._density=density
+
+    @property
+    def name(self):
+        """Returns the name of the material"""
+        return self._name
+
+    @property
+    def density(self):
+        """Returns the density of the material in kg/m3"""
+        return self._density
